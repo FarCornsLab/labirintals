0a18a3b34245ca6d8bac6975cbad4e53a6db82c0
Danila Goriachkin <dndv279@gmail.com>
2021-01-07 19:53:51 +1000

    Main returned.

--- AI/main.cpp
+++ AI/main.cpp
@@ -1,30 +1,17 @@
 #include "Bot.h"
 
-#include "Commands.h"
-
 int main() {
-//    Bot bot("conf.json");
-//    auto err = bot.getError();
-//    if (err.has_value()) {
-//        return -static_cast<unsigned int>(err.value());
-//    }
-//
-//    bot.run();
-//    err = bot.getError();
-//    if (err.has_value()) {
-//        return -static_cast<unsigned int>(err.value());
-//    }
-//
-//    return 0;
+    Bot bot("conf.json");
+    auto err = bot.getError();
+    if (err.has_value()) {
+        return -static_cast<unsigned int>(err.value());
+    }
 
+    bot.run();
+    err = bot.getError();
+    if (err.has_value()) {
+        return -static_cast<unsigned int>(err.value());
+    }
 
-    auto ans = s_cmd::ServerAnswer::get<s_cmd::ConnectionAnswer>("{\n"
-                                                        "    \"cmd\": \"connection_answer\",\n"
-                                                        "    \"params\": {\n"
-                                                        "        \"oid\": 2716057,\n"
-                                                        "        \"сid\": 1729\n"
-                                                        "    }\n"
-                                                        "}");
-
-    std::cout << ans->oid << std::endl;
+    return 0;
 }
