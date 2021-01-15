2c4269aeb0cb928868979b5f0b3b1a892275966e
kenny5660 <32442117+kenny5660@users.noreply.github.com>
2021-01-15 23:11:29 +1000

    add direct by ip connect

--- Client/FindServerMenu.py
+++ Client/FindServerMenu.py
@@ -33,6 +33,19 @@ class FindServerMenu(Scene):
                                             container=self.panel,
                                             manager=self.ui_manager)
         self.btn_pressed_call_backs.append((self.btn_connect,self.btn_connect_pressed))
+
+        self.btn_direct_ip = pygame_gui.elements.UIButton(
+                                            relative_rect=pygame.Rect((430,350),(120,25)),
+                                            text='Connect by IP',
+                                            container=self.panel,
+                                            manager=self.ui_manager)
+        self.btn_pressed_call_backs.append((self.btn_direct_ip,self.btn_direct_ip_pressed))
+
+        self.txt_ip = pygame_gui.elements.UITextEntryLine( 
+                                            relative_rect=pygame.Rect((280,350),(150,25)),
+                                            container=self.panel,
+                                            manager=self.ui_manager)
+
         sel_item_list  = [str(i+1)+". "+Core.core.config["server_list"][i]["name"] for i in range(len(Core.core.config["server_list"]))]
         self.server_list = pygame_gui.elements.UISelectionList(pygame.Rect(50, 50, 500, 300),
                         item_list=sel_item_list,
@@ -43,19 +56,27 @@ class FindServerMenu(Scene):
     def btn_back_pressed(self, event):
         Core.core.load_scene("MainMenu")
 
-    def btn_connect_pressed(self, event):
-        sel = self.server_list.get_single_selection()
-        if sel != None:
-            print("Connect to "+ sel )
-            num = int(sel.split(sep=".")[0]) - 1
-            server = Core.core.config["server_list"][num]
-            try:
-                Core.core.net_manager.connect(server["ip"],server["port"])
+    def btn_direct_ip_pressed(self, event):
+        txt = self.txt_ip.get_text().split(":")
+        self.connect_to(txt[0],int(txt[1]))
+
+    def connect_to(self,ip,port):
+        try:
+                Core.core.net_manager.connect(ip,port)
                 Core.core.load_scene("WaitStartGameMenu")
-            except ConnectionError as err:
+        except ConnectionError as err:
                 print("Error"+ err.strerror)
                 self.err_msg = pygame_gui.windows.UIMessageWindow(
                     rect=pygame.Rect(100, 100, 350, 200),
                     html_message="Connection Error <br>"+ err.strerror,
                     manager=self.ui_manager,
                     window_title="Error")
+
+    def btn_connect_pressed(self, event):
+        sel = self.server_list.get_single_selection()
+        if sel != None:
+            print("Connect to "+ sel )
+            num = int(sel.split(sep=".")[0]) - 1
+            server = Core.core.config["server_list"][num]
+            self.connect_to(server["ip"],server["port"])
+            
